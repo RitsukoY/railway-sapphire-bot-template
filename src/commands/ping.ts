610d757a9f65ae580b6bbf68a524ac3df6e9a2c5
RitsukoY <ritsuko1435yoshio@gmail.com>
2023-05-07 19:04:53 -0300

    Update ping.ts
    Edit ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -15,7 +15,14 @@ export class PingCommand extends Command {
 	}
 
 	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Pong! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)' });
-		return msg;
+		const msg = await interaction.reply({ content: `Ping?`, ephemeral: true, fetchReply: true });
+
+    	if (isMessageInstance(msg)) {
+      		const diff = msg.createdTimestamp - interaction.createdTimestamp;
+      		const ping = Math.round(this.container.client.ws.ping);
+      		return interaction.editReply(`Pong 🏓! (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
+    	}
+
+		return interaction.editReply('Failed to retrieve ping :(');
 	}
 }
